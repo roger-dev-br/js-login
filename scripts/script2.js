2f920f98ba38e5abf5f50de9fe54ee458864c6b3
Roger Medeiros <rogermedeiros@outlook.com>
2020-10-27 22:29:21 -0300

    criada rotina de busca de usuario para login

--- scripts/script2.js
+++ scripts/script2.js
@@ -2,20 +2,32 @@ var email = document.getElementById("email");
 var password = document.getElementById("password");
 var foto = document.getElementById("foto");
 
-function entrar(event) {
-    event.preventDefault();
-
+function buscaUsuario(identificacao) {
     // Busco o indice do usuário na lista
-    var posicao = usersList.findIndex(user => user.email === email.value);
+    var posicao = usersList.findIndex(
+        user => user.email === identificacao ||
+            user.login.username === identificacao
+    );
 
     // Verifico se o usuário existe
     if (posicao === -1) {
+        return null;
+    }
+
+    return usersList[posicao];
+}
+
+function entrar(event) {
+    event.preventDefault();
+
+    // Busco o usuario
+    var usuario = buscaUsuario(email.value);
+
+    // Verifico se o usuário existe
+    if (usuario === null) {
         alert("Usuário não encontrado");
         return;
     }
-
-    // coloco o usuário selecionado em uma variável
-    var usuario = usersList[posicao];
 
     // Valido se senha está incorreta
     if (usuario.login.password !== password.value) {
@@ -35,20 +47,15 @@ function trocafoto() {
         return;
     }
 
-    // Busco o indice do usuário na lista
-    var posicao = usersList.findIndex(
-        user => user.email === email.value || 
-            user.login.username === email.value
-    );
-
+    var usuario = buscaUsuario(email.value);
+    
     // Verifico se o usuário existe
-    if (posicao === -1) {
+    if (usuario === null) {
         alert("Usuário não encontrado");
         return;
     }
 
     // coloco o usuário selecionado em uma variável
-    var usuario = usersList[posicao];
     email.value = usuario.email;
     foto.src = usuario.picture.large;
 }
